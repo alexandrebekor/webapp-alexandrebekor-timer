e193746e50f967eeb196ccd6aaaaa90694176c13
Alexandre Bekor <staff@agenciabekor.com>
2023-02-11 18:58:52 -0300

    :rocket: add header and style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import { HeaderContainer } from './styles'
+import { Alarm, Scroll } from 'phosphor-react'
+
+import logoBekor from '../../assets/bekor.svg'
+import { NavLink } from 'react-router-dom'
+
 export const Header = () => {
-  return <header>Header</header>
+  return (
+    <HeaderContainer>
+      <img src={logoBekor} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Alarm size={24} />
+        </NavLink>
+        <NavLink to="/history" title="History">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
