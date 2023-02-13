06b52732e55121f264d7fe965f21d1c4a32b03c3
Alexandre Bekor <staff@agenciabekor.com>
2023-02-13 16:40:36 -0300

    :rocket: added and styled Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  SeparatorContainer
+} from './styles'
+
+export const Home = () => {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <SeparatorContainer>:</SeparatorContainer>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button>
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
